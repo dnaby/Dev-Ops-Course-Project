56664af782ef0310a1a5686b2a9896de5452e28c
Abk-coder <dkhadre@ept.sn>
2023-09-12 15:20:11 +0000

    configuration de la bd et ajout des deux endpoinds

---
+++ devopsDjangoApp/devops_backend/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class User(models.Model):
+    username = models.CharField(max_length=200, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
